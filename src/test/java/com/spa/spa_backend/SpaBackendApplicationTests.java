677f90e940279415d718d18c2b89a99856fb2e23
Damian Guano <damian.guano80@gmail.com>
2026-08-11 20:03:44 -0500

    Initial commit: Spring Boot 4.0.7 + Web+ Mongo DB + DevTools

---
+++ src/test/java/com/spa/spa_backend/SpaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spa.spa_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpaBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
